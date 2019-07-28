c61d589b133db7b552e5b9a1304158174490eef2
Jairo Avila <javila@novopayment.com>
2019-07-28 16:21:44 -0500

    Spash and Welcome screen added

---
+++ app/src/main/java/com/pma/ekaa/SplashActivity.java
@@ -0,0 +1,41 @@
+package com.pma.ekaa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    ImageView logo;
+    TextView webname;
+    Animation fromtop,fromBottom;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        logo = findViewById(R.id.pmaLogo);
+        webname = findViewById(R.id.pmaText);
+
+        fromtop = AnimationUtils.loadAnimation(this,R.anim.fromtop);
+        fromBottom = AnimationUtils.loadAnimation(this,R.anim.frombottom);
+
+        logo.setAnimation(fromtop);
+        webname.setAnimation(fromBottom);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, WelcomeActivity.class);
+                startActivity(intent);
+            }
+        },2000);
+    }
+}
